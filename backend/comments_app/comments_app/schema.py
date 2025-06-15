db70ed71c3bc918c3adf0e2ecfad4c095c5ea113
Sergey Marich <sgc20081@gmail.com>
2025-06-16 02:28:20 +0300

    Register RegisterUser schema in basic schema

--- backend/comments_app/comments_app/schema.py
+++ backend/comments_app/comments_app/schema.py
@@ -1,6 +1,7 @@
 import graphene
 
 from comments_service.schema import CommentType, CreateComment
+from auth_service.schema import RegisterUser
 
 class Query(graphene.ObjectType):
     all_comments = graphene.List(CommentType)
@@ -10,6 +11,7 @@ class Query(graphene.ObjectType):
 
 class Mutation(graphene.ObjectType):
     create_comment = CreateComment.Field()
+    register_user = RegisterUser.field()
     
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
