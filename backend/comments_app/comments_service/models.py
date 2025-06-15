e30448505c7f62619b3977987d15fcac2cf92043
Sergey Marich <sgc20081@gmail.com>
2025-06-15 17:27:12 +0300

    Creating data models

--- backend/comments_app/comments_service/models.py
+++ backend/comments_app/comments_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    username = models.CharField(max_length=15)
+
+class Comment(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+
+class ChildComment(Comment):
+    parent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='child_comments')
