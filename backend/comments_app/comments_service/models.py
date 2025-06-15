a3fdb72ce8bf5371f6e74914f8c880afa1ec6675
Sergey Marich <sgc20081@gmail.com>
2025-06-15 18:32:48 +0300

    Add fields (images, files, parent) to Comment, homepage to CustomUser; add __str__, __repr__ methods

--- backend/comments_app/comments_service/models.py
+++ backend/comments_app/comments_service/models.py
@@ -4,11 +4,32 @@ from django.db import models
 
 class CustomUser(models.Model):
     username = models.CharField(max_length=15)
+    homepage = models.URLField(blank=True)
+
+    def __str__(self):
+        return (f'({self.__class__.__name__}: username = "{self.username}", homepage = "{self.homepage}")')
+    
+    def __repr__(self):
+        return (f'({self.__class__.__name__}: username = "{self.username}", homepage = "{self.homepage}")')
 
 class Comment(models.Model):
-    date_created = models.DateTimeField(auto_now_add=True)
-    text = models.TextField()
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='childs')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    file = models.FileField(upload_to='files/', null=True, blank=True)
 
-class ChildComment(Comment):
-    parent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='child_comments')
+    def __str__(self):
+        parent = None
+        
+        if self.parent:
+            parent = self.parent.text
+        return (f'({self.__class__.__name__}: author = "{self.author.username}", text = "{self.text}", date = "{self.date_created}", parent = "{parent}")')
+    
+    def __repr__(self):
+        parent = None
+
+        if self.parent:
+            parent = self.parent.text
+        return (f'({self.__class__.__name__}: author = "{self.author.username}", text = "{self.text}", date = "{self.date_created}", parent = "{parent}")')
